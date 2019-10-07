89eb0f49280835131d905cc19d351b1b7148e45c
Thomas Butler <thomas@butler.software>
2019-10-07 14:41:53 -0400

    util functions from electron-redux-store

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,2 +1,10 @@
+import fillShape from './fill-shape';
+import objectDifference from './object-difference';
+import objectMerge from './object-merge';
+
+
 export * from './utilTypes';
 export * from './decorators';
+
+
+export {fillShape, objectDifference, objectMerge};
