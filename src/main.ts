168caea305fc0b04c4e69a0b6a70c442dae0da39
Thomas Butler <thomas@butler.software>
2019-10-11 21:14:58 -0400

    initial

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { ipcMain, WebContents } from 'electron';
+import { StoreEnhancer } from 'redux';
+
+export const proxyEnhancer: StoreEnhancer = createStore => {
+  return (reducer, preloadedState) => {
+    const store = createStore(reducer, preloadedState);
+    const subscribers = new Set<WebContents>();
+
+    store.subscribe(() => {
+      Array.from(subscribers).forEach(webContents => webContents.send('store-update'));
+    });
+
+    ipcMain.on('dispatch', (event, { action }) => {
+      event.returnValue = store.dispatch(action);
+    });
+    ipcMain.on('getState', event => {
+      event.returnValue = store.getState();
+    });
+    ipcMain.on('subscribe', event => {
+      subscribers.add(event.sender);
+    });
+
+    return store;
+  };
+};
