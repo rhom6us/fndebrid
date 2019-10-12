fbb6c2aeefb61c91b9680fd4416e3fb752829aa0
Thomas Butler <thomas@butler.software>
2019-10-11 20:59:51 -0400

    new file selection module

---
+++ src/main/file-selection.ts
@@ -0,0 +1,44 @@
+
+import path from 'path';
+import { format as formatUrl } from 'url'
+import { BrowserWindow } from 'electron';
+const isDev = process.env.NODE_ENV !== 'production'
+const DEBUG = process.env.DEBUG;
+
+let window: BrowserWindow | undefined;
+const route = 'FileSelect';
+export function showWindow(){
+  if(window){
+    window.focus();
+    return;
+  }
+  window = new BrowserWindow({
+    webPreferences:{
+      nodeIntegration: true
+    },
+    frame: false,
+    width: 600,
+    height: 600,
+    resizable: false,
+    alwaysOnTop: true,
+    fullscreenable: false,
+    skipTaskbar: true,
+
+  });
+
+  window.on('close', ()=>{
+    window = undefined;
+  });
+  
+  if (isDev) {
+    window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}/?route=${route}`);
+  }
+  else {
+    window.loadURL(formatUrl({
+      pathname: path.join(__dirname, 'index.html'),
+      query: { route },
+      protocol: 'file',
+      slashes: true,
+    }));
+  }
+}
