166ea7e3a2736645d5e7ee1652e9e3fc1259d491
Thomas Butler <thomas@butler.software>
2019-11-04 01:57:51 -0500

    rename 'torrents' actions to 'real-debrid'

--- packages/@fndebrid/store/real-debrid/actions.ts
+++ packages/@fndebrid/store/real-debrid/actions.ts
@@ -2,48 +2,53 @@ import {ExtendedTorrent, FileId, MagnetLink, Torrent, TorrentHash, TorrentId} fr
 import {createAction, createAsyncAction} from 'typesafe-actions';
 import {JobId} from './state';
 
-export const completeJob = createAction('torrents/completeJob', action => (jobId: JobId) => action({jobId}));
-export const cancelJob = createAction('torrents/cancelJob', action => (jobId: JobId) => action({jobId}));
+export const completeJob = createAction('real-debrid/completeJob', action => (jobId: JobId) => action({jobId}));
+export const cancelJob = createAction('real-debrid/cancelJob', action => (jobId: JobId) => action({jobId}));
 export const deleteTorrent = createAsyncAction(
-  'torrents/deleteTorrent/request',
-  'torrents/deleteTorrent/success',
-  'torrents/deleteTorrent/error',
+  'real-debrid/deleteTorrent/request',
+  'real-debrid/deleteTorrent/success',
+  'real-debrid/deleteTorrent/error',
 )<TorrentId, TorrentId, string>();
 export const selectFiles = createAsyncAction(
-  'torrents/selectFiles/request',
-  'torrents/selectFiles/success',
-  'torrents/selectFiles/error',
+  'real-debrid/selectFiles/request',
+  'real-debrid/selectFiles/success',
+  'real-debrid/selectFiles/error',
 )<[TorrentId, FileId[]], undefined, Error>();
 
 export const fetchTorrent = createAsyncAction(
-  'torrents/fetch/request',
-  'torrents/fetch/success',
-  'torrents/fetch/error',
+  'real-debrid/fetch/request',
+  'real-debrid/fetch/success',
+  'real-debrid/fetch/error',
 )<TorrentId, ExtendedTorrent, string>();
 
 export const fetchTorrents = createAsyncAction(
-  'torrents/fetch-all/request',
-  'torrents/fetch-all/success',
-  'torrents/fetch-all/error',
+  'real-debrid/fetch-all/request',
+  'real-debrid/fetch-all/success',
+  'real-debrid/fetch-all/error',
 )<undefined, Torrent[], string>();
 
 export const addMagnet = createAsyncAction(
-  'torrents/add-magnet/request',
-  'torrents/add-magnet/success',
-  'torrents/add-magnet/error',
+  'real-debrid/add-magnet/request',
+  'real-debrid/add-magnet/success',
+  'real-debrid/add-magnet/error',
 )<[MagnetLink, JobId], [TorrentId, JobId], string>();
 
 export const addTorrentFile = createAsyncAction(
-  'torrents/add-torrent-file/request',
-  'torrents/add-torrent-file/success',
-  'torrents/add-torrent-file/error',
+  'real-debrid/add-torrent-file/request',
+  'real-debrid/add-torrent-file/success',
+  'real-debrid/add-torrent-file/error',
 )<[string, JobId], [TorrentId, JobId], string>();
 export const getCaches = createAsyncAction(
-  'torrents/get-caches/request',
-  'torrents/get-caches/success',
-  'torrents/get-caches/error',
+  'real-debrid/get-caches/request',
+  'real-debrid/get-caches/success',
+  'real-debrid/get-caches/error',
 )<[TorrentHash, JobId], [FileId[][], JobId], Error>();
 
-export const setInfoHash = createAction('torrents/set-info-hash', action => (jobId: JobId, infoHash: TorrentHash) =>
+export const pollTorrents = createAsyncAction(
+  'real-debrid/pollTorrents/request',
+  'real-debrid/pollTorrents/success',
+  'real-debrid/pollTorrents/error',
+)<{interval: number}, undefined, Error>();
+export const setInfoHash = createAction('real-debrid/set-info-hash', action => (jobId: JobId, infoHash: TorrentHash) =>
   action({jobId, infoHash}),
 );
