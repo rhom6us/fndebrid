6f0a07c340118f5efbc061a4d86a70b1d5688205
Thomas Butler <>
2019-11-12 12:31:13 -0500

    build files for previous

--- packages/@fndebrid/webpack/lib/fnbuild.js
+++ packages/@fndebrid/webpack/lib/fnbuild.js
@@ -7,5 +7,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var compile_1 = __importDefault(require("./compile"));
 var _a = process.argv.reverse().slice(0, 2), env = _a[0], app = _a[1];
 // ...process.argv.slice(process.argv.length - 3, process.argv.length-1)
-compile_1.default('fnbuild', app, env);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZm5idWlsZC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uL3NyYy9mbmJ1aWxkLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7OztBQUVBLHNEQUFzQztBQUdoQyxJQUFBLHVDQUFxRSxFQUFwRSxXQUFHLEVBQUUsV0FBK0QsQ0FBQztBQUM1RSx3RUFBd0U7QUFDeEUsaUJBQWEsQ0FBQyxTQUFTLEVBQUUsR0FBRyxFQUFFLEdBQUcsQ0FBQyxDQUFDIn0=
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+compile_1.default('fnbuild', app, process.env.NODE_ENV || 'development');
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZm5idWlsZC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uL3NyYy9mbmJ1aWxkLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7OztBQUVBLHNEQUFzQztBQUdoQyxJQUFBLHVDQUFxRSxFQUFwRSxXQUFHLEVBQUUsV0FBK0QsQ0FBQztBQUM1RSx3RUFBd0U7QUFDeEUsT0FBTyxDQUFDLEdBQUcsQ0FBQyxRQUFRLEdBQUcsT0FBTyxDQUFDLEdBQUcsQ0FBQyxRQUFRLElBQUksYUFBYSxDQUFDO0FBQzdELGlCQUFhLENBQUMsU0FBUyxFQUFFLEdBQUcsRUFBRSxPQUFPLENBQUMsR0FBRyxDQUFDLFFBQVEsSUFBSSxhQUFhLENBQUMsQ0FBQyJ9
