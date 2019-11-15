77e7c2226c37029fae644ad21e4568b8299444ba
Thomas Butler <>
2019-11-15 14:49:52 -0500

    fixed getDispatcher for function only use-case

--- packages/@fndebrid/store/dispatcher.ts
+++ packages/@fndebrid/store/dispatcher.ts
@@ -1,9 +1,11 @@
-import {ActionCreator as TsActionsActionCreator, TypeConstant} from 'typesafe-actions';
-import {Dispatch, Store} from './';
+import {assertNever, isFunction} from '@fndebrid/core';
+import {ActionCreator as TsActionsActionCreator, ActionType, TypeConstant} from 'typesafe-actions';
+import {FnAction, FnDispatch, FnStore} from './';
 import * as actions from './actions';
 
-type ActionCreator = TsActionsActionCreator<TypeConstant>;
-
+type ActionCreator = (...args: any[]) => FnAction;
+type huh = TsActionsActionCreator<TypeConstant>;
+type wow = ActionType<typeof actions>;
 // type DispatchedActionCreator<TActionCreator extends AnyActionCreator> = (...args: Parameters<TActionCreator>) => void;
 // type DispatchedMap<TMap extends Record<string, ActionCreatorAny>> = {
 //   [K in keyof TMap]: Dispatched<TMap[K]>
@@ -11,30 +13,53 @@ type ActionCreator = TsActionsActionCreator<TypeConstant>;
 // type Dispatched<TActionCreatorOrMap> = TActionCreatorOrMap extends ActionCreatorAny ? DispatchedActionCreator<TActionCreatorOrMap> :
 //   TActionCreatorOrMap extends Record<string, any> ? DispatchedMap<TActionCreatorOrMap> :
 //   TActionCreatorOrMap extends infer R ? never : never;
+// type ActionCreatorMap = ActionCreator | Record<string, ActionCreator | Record<string, ActionCreator | Record<string, ActionCreator | Record<string, ActionCreator | Record<string, ActionCreator | Record<string, ActionCreator | Record<string, ActionCreator | Record<string, ActionCreator>>>>>>>>;
+type ActionCreatorOrMap =
+  | ActionCreator
+  | {
+      [key: string]: ActionCreatorOrMap;
+    };
+function isActionCreator(value: ActionCreatorOrMap): value is ActionCreator {
+  return isFunction(value);
+}
 type Dispatched<TActionCreatorOrMap> = TActionCreatorOrMap extends ActionCreator
   ? (...args: Parameters<TActionCreatorOrMap>) => void
-  : TActionCreatorOrMap extends Record<string, any>
+  : TActionCreatorOrMap extends Record<string, ActionCreatorOrMap>
   ? {[K in keyof TActionCreatorOrMap]: Dispatched<TActionCreatorOrMap[K]>}
   : never;
 
-function applyDispatch<TMap extends Record<string, any>>(dispatch: Dispatch, map: TMap): Dispatched<TMap> {
-  const result: any = {};
-  for (const key in map) {
-    const creatorOrMap = map[key];
-    if (typeof creatorOrMap == 'function') {
-      result[key] = (...args: Parameters<typeof creatorOrMap>) => dispatch(creatorOrMap(...args));
-    } else {
-      result[key] = applyDispatch(dispatch, creatorOrMap);
-    }
+function applyDispatch<TMap extends ActionCreatorOrMap>(dispatch: FnDispatch, map: TMap): Dispatched<TMap> {
+  if (isFunction(map)) {
+    return ((...args: any[]) => dispatch((map as any)(...args))) as any;
   }
-  return result;
+  return Object.keys(map).reduce(
+    (result, key) => ({
+      ...result,
+      [key]: applyDispatch(dispatch, (map as any)[key]),
+    }),
+    {},
+  ) as any;
+  // return Object.keys(map).reduce((result, key) => ({
+  //   ...result,
+  //   [key]: isFunction(map[key])
+  // }), {} as Dispatched<TMap>);
+  // return Object.keys(map).reduce((result, key) => ({
+  //     ...result,
+  //     [key]: isFunction( map[key])
+  //       ? (...args) => dispatch( map[key](...args))
+  //       : applyDispatch(dispatch,  map[key])
+  //   }), {} as Dispatched<TMap>);
 }
 
-export type Dispatcher = Dispatched<typeof actions>;
+export type FnDispatcher = Dispatched<typeof actions>;
 
-const dispatchers = new Map<Dispatch, Dispatcher>();
-export function getDispatcher(dispatchOrStore: Store | Dispatch): Dispatcher {
-  const dispatch: Dispatch = typeof dispatchOrStore == 'function' ? dispatchOrStore : dispatchOrStore.dispatch;
+const dispatchers = new Map<FnDispatch, FnDispatcher>();
+export function getDispatcher(dispatchOrStore: FnStore | FnDispatch): FnDispatcher {
+  const dispatch: FnDispatch = isFunction(dispatchOrStore)
+    ? dispatchOrStore
+    : 'dispatch' in dispatchOrStore
+    ? dispatchOrStore.dispatch
+    : assertNever(dispatchOrStore);
 
   if (!dispatchers.has(dispatch)) {
     dispatchers.set(dispatch, applyDispatch(dispatch, actions));
