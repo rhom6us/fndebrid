5ab594d21e3a9046c15d9d45874b4fa1e9993c32
Thomas Butler <>
2020-04-18 05:47:15 -0400

    formatting -- no functinoal change

--- packages/@fndebrid/core/FnError.ts
+++ packages/@fndebrid/core/FnError.ts
@@ -7,13 +7,14 @@ export class FnError extends Error {
     super(`${message}${data ? ` (${JSON.stringify(data)})` : ''}`);
     this.name = new.target.name;
 
-    //prior to es2015 (e.g. es5) the Error constructor broke the prototype chain, hence the following:
+    // prior to es2015 (e.g. es5) the Error constructor broke the prototype chain, hence the following:
     if (!(this instanceof FnError)) {
       Object.setPrototypeOf(this, new.target.prototype);
     }
   }
 }
 
+// tslint:disable: max-classes-per-file
 export class OperationFailedError extends FnError {}
 export class InvalidOperationError extends FnError {}
 export class UnexpectedStateError extends FnError {}
